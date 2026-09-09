6dbf2a8c710f97c697209dc52ac141791dfb0f66
Stacy-Lena <wyfqssp8jt@privaterelay.appleid.com>
2026-09-09 22:15:11 +0300

    LENA area of triangle

--- Chapter_02/triangle_area.cpp
+++ Chapter_02/triangle_area.cpp
@@ -4,3 +4,30 @@
 // Area = sqrt(s(s-side1)(s-side2)(s-side3))
 // Example: Enter three points for a triangle: 1.5, -3.4, 4.6, 5, 9.5, -3.4 
 // The area of the triangle is 33.6
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int main() {
+    double x1, y1, x2, y2, x3, y3;
+
+    cout << "Enter three points for a triangle: ";
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+
+    double side1 = distance(x1, y1, x2, y2);
+    double side2 = distance(x2, y2, x3, y3);
+    double side3 = distance(x3, y3, x1, y1);
+
+    
+    double s = (side1 + side2 + side3) / 2;
+    double area = sqrt(s * (s - side1) * (s - side2) * (s - side3));
+
+    cout << "The area of the triangle is " << area << endl;
+
+    return 0;
+}
